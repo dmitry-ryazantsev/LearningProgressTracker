70242c6af70dac5157f8caf5ee2d9e226e34ea9d
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2024-04-18 12:33:49 +0300

    Implement adding subject points

--- Learning Progress Tracker (Python)/task/task.py
+++ Learning Progress Tracker (Python)/task/task.py
@@ -18,12 +18,31 @@ class LearningProgressTracker:
             print("The student has been added.")
 
     def list_students(self):
-        if self.students == {}:
+        if not self.students:
             print("No students found.")
         else:
             print("Students:")
             for student in self.students:
                 print(student["id"])
+
+    def add_points(self, points):
+        if not self.validate_points(points):
+            print("Incorrect points format.")
+            return
+
+        data = self.convert_points_string_into_int_array(points)
+        student_id = data[0]
+        points_to_add = data[1:]
+
+        for student in self.students:
+            if student["id"] == student_id:
+                subjects = student["subjects"]
+                for subject, pts in zip(["Python", "DSA", "Databases", "Flask"], points_to_add):
+                    subjects[subject] += pts
+                print("Points updated.")
+                return
+
+        print(f"No student is found for id={student_id}.")
 
     def validate_student_credentials(self, credentials):
         parts = credentials.split()
@@ -60,14 +79,14 @@ class LearningProgressTracker:
         # - Hyphens and apostrophes cannot be the first or the last characters
         # - Hyphens and apostrophes cannot be adjacent to each other
         # - Must be at least two characters long
-        pattern = r'^[a-z](?!.*[-\']{2})[a-z\' -]*[a-z]$'
-        return bool(re.match(pattern, name, re.IGNORECASE))
+        name_pattern = r'^[a-z](?!.*[-\']{2})[a-z\' -]*[a-z]$'
+        return re.match(name_pattern, name, re.IGNORECASE)
 
     @staticmethod
     def validate_email(email):
         # Should contain name, the @ symbol, and domain
-        pattern = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
-        return bool(re.match(pattern, email))
+        email_pattern = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
+        return re.match(email_pattern, email)
 
     def is_email_unique(self, email):
         for student in self.students:
@@ -77,6 +96,16 @@ class LearningProgressTracker:
                 return False
 
         return True
+
+    @staticmethod
+    def validate_points(points):
+        points_pattern = r'^\d+( \d+){4}$'
+        return re.match(points_pattern, points)
+
+    @staticmethod
+    def convert_points_string_into_int_array(points):
+        modified_points = [int(x) for x in points.split()]
+        return modified_points
 
 
 class UserMenu:
@@ -107,6 +136,15 @@ class UserMenu:
     def list_students_command(self):
         self.tracker.list_students()
 
+    def add_points_command(self):
+        print("Enter an id and points or 'back' to return:")
+        while True:
+            points = input()
+            if points == "back":
+                break
+            else:
+                self.tracker.add_points(points)
+
     def display_menu(self):
         self.greet_user()
         while True:
@@ -118,6 +156,8 @@ class UserMenu:
                 self.add_students_command()
             elif user_command == "list":
                 self.list_students_command()
+            elif user_command == "add points":
+                self.add_points_command()
             elif user_command == "back":
                 print("Enter 'exit' to exit the program.")
             elif user_command.strip() == "":
